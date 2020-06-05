114b1801d8c9292f2f3fdd277d42089f57d3e7e1
Manu Manoj <manumanoj4540@gmail.com>
2020-06-05 21:54:01 +0530

    Updates

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,23 +18,7 @@ class Menu extends Component {
             console.log(dish.name);
         }
         
-     renderDish(dish) 
-        {
-            if (dish != null)
-                return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                          <CardTitle>{dish.name}</CardTitle>
-                          <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            else
-                return(
-                    <div></div>
-                );
-        }
+     
 
     render() {
         const menu = this.props.dishes.map((dish) => {
--- src/components/test.js
+++ src/components/test.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Test extends Component {
     constructor(props)
     {
         super(props)
         this.state = {
-
+            
         };
+        
     }
     renderDish(dish) 
-        {
+        { console.log(this.props.dish);
             if (dish != null)
                 return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                          <CardTitle>{dish.name}</CardTitle>
-                          <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
+                    
+                        <Card className= "col-12 col-md-5">
+                            <CardImg top src={dish.image} alt={dish.name} />
+                            <CardBody>
+                              <CardTitle>{dish.name}</CardTitle>
+                              <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                 );
             else
                 return(
                     <div></div>
                 );
         }
 
+     
+
     render ()
-    {
+    { 
+       
+
+        
+        
        return (
-        <div>
-        { this.renderDish(this.props.dish) }
-        </div>
+        
+           
+                <div>
+                     { this.renderDish(this.props.dish) }
+                </div>
+                
+            
+       
        );
         
        

05de5d5953a6c33bcd9c5e71e7ed2a566a531bad
Manu Manoj <manumanoj4540@gmail.com>
2020-06-06 15:08:12 +0530

    implemented description

--- src/components/test.js
+++ src/components/test.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import '../App.css'
 
 class Test extends Component {
     constructor(props)
@@ -8,47 +9,56 @@ class Test extends Component {
         this.state = {
             
         };
-        
     }
     renderDish(dish) 
-        { console.log(this.props.dish);
+        { 
+            console.log(dish);
             if (dish != null)
-                return(
-                    
-                        <Card className= "col-12 col-md-5">
+                {   console.log(dish.comments);
+                    let comslist = dish.comments.map((cur)=>{
+                        return(
+                            <div className="adj" key={cur.id}>
+                                <p>{cur.comment} <br/>
+                                   Rating : {cur.rating} <br/>
+                                   Author : {cur.author}
+                                </p>
+                            </div>                           
+                        );                       
+                    });
+                    //console.log(comslist);
+                    return(
+                    <div className="row">
+                        <Card className= "col-12 col-md-5" key={dish.id}>
                             <CardImg top src={dish.image} alt={dish.name} />
                             <CardBody>
                               <CardTitle>{dish.name}</CardTitle>
                               <CardText>{dish.description}</CardText>
                             </CardBody>
                         </Card>
+
+                        <div className="col-12 col-md text-center">
+                            <h2>Comments</h2>
+                            <div>{comslist}</div>
+                        </div>
+                    </div>
                  );
+                }
             else
                 return(
                     <div></div>
                 );
         }
 
-     
+    
 
     render ()
     { 
-       
-
-        
-        
+    
        return (
-        
-           
                 <div>
                      { this.renderDish(this.props.dish) }
                 </div>
-                
-            
-       
-       );
-        
-       
+              );       
     }
 
 }

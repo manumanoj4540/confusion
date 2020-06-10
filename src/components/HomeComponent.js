46aaede9607c6048e8072b7431e37999678c27d8
Manu Manoj <manumanoj4540@gmail.com>
2020-06-10 17:17:29 +0530

    Added Home and Contact pages

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Card, CardTitle, CardBody, CardImg, CardText, CardSubtitle} from 'reactstrap'
 
-function Home(props) {
+
+class Home extends Component {
+
+  RenderCard = (obj) => {
+    if(obj)
     return(
+    <Card className= "mt-2 mb-2">
+       <CardImg src ={obj.image} alt={obj.name}/>
+       <CardBody>
+         <CardTitle><h3><b>{obj.name}</b></h3></CardTitle>
+         {obj.abbr ? <CardSubtitle><h4>{obj.designation}</h4></CardSubtitle> : null }
+         <CardText>{obj.description}</CardText>
+       </CardBody>
+     </Card>);
+  }
+
+  render()
+    {
+      return(
       <div className="container">
-        <h4>Home</h4>
+        <div className="row">
+          <div className= "col-12 col-md-4 ">{this.RenderCard(this.props.dish)}</div>
+          <div className= "col-12 col-md-4 ">{this.RenderCard(this.props.promo)}</div>
+          <div className= "col-12 col-md-4 ">{this.RenderCard(this.props.leader)}</div>
+        </div>
+       
+        
       </div>
-    );
+    );}
 }
 
 export default Home;   
+

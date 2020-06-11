d42c4b80306f713b9c0e17b25e9317a9ac2de677
Manu Manoj <manumanoj4540@gmail.com>
2020-06-11 15:37:54 +0530

    Introduced Aboutus Component to the application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { COMMENTS } from '../shared/comments'
 import { LEADERS } from '../shared/leaders'
 import { PROMOTIONS } from '../shared/promotions'
 import { Switch, Route, Redirect } from 'react-router-dom';
+import About from './AboutComponent' ;
 import '../App.css'
 
 class Main extends Component {
@@ -45,11 +46,18 @@ class Main extends Component {
       );
     }
 
+    const AboutPage = () => {
+      return(
+        <About leaders={this.state.leaders} />
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
+              <Route path= '/aboutus' component = {AboutPage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path = '/menu/:dishId' component = {SelectedDish} /> 
               <Route path ='/contactus' component={Contact}/>
